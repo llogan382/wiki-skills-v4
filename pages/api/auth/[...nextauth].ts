77dd2c8c578ca7c7c054993280c7782a8583cd9e
Luke <llogan382@gmail.com>
2022-12-29 15:35:57 -0500

    updates netlify plugins

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,6 +6,7 @@ import GitHubProvider from 'next-auth/providers/github'
 import prisma from '../../../lib/prisma'
 import { url } from "inspector";
 import NextAuth, { User as NextAuthUser } from 'next-auth'
+import { randomUUID, randomBytes } from "crypto";
 interface NextAuthUserWithStringId extends NextAuthUser {
 id: string
 }
@@ -25,6 +26,29 @@ export const authOptions = {
       },
     }),
   ],
+  session: {
+    // Choose how you want to save the user session.
+    // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
+    // If you use an `adapter` however, we default it to `"database"` instead.
+    // You can still force a JWT session by explicitly defining `"jwt"`.
+    // When using `"database"`, the session cookie will only contain a `sessionToken` value,
+    // which is used to look up the session in the database.
+    strategy: "database",
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
+
+    // The session token is usually either a random UUID or string, however if you
+    // need a more customized session token string, you can define your own generate function.
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex")
+    }
+  },
   adapter: PrismaAdapter(prisma),
   debug: true,
   useSecureCookies: false,
@@ -34,7 +58,7 @@ export const authOptions = {
       return session
     }
   },
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: "5OzLitvYL0eebk4GEDmKFA0c7knRKI8FR/fh6Chawjc="
 };
 
 
