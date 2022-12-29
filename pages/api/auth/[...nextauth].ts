f9f3b198f6a724a7adf4fcc777f345df87ee785d
Luke <llogan382@gmail.com>
2022-12-29 14:31:51 -0500

    uses-prisma

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -27,35 +27,9 @@ export const authOptions = {
     }),
   ],
   debug: true,
-  logger: {
-    error(code, metadata) {
-      console.log(code, metadata)
-    },
-    warn(code) {
-      log.warn(code)
-    },
-    debug(code, metadata) {
-      log.debug(code, metadata)
-    }
-  },
   useSecureCookies: false,
   callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log(user)
-      return true
-    },
-    async jwt({ token, account, profile }) {
-      // Persist the OAuth access_token and or the user id to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-        token.id = profile.id
-      }
-      return token
-    },
     async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      session.accessToken = token.accessToken
-      session.user.id = token.id
 
       return session
     }
